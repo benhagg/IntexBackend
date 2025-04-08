45e8745191af6f0ae793e6a7d6631f5e8e065c2a
barlee1 <kyleeb2255@gmail.com>
2025-04-08 14:00:51 -0600

    updated the database and the controllers for the admin page

--- Models/MovieTitleDto.cs
+++ Models/MovieTitleDto.cs
@@ -1,10 +1,14 @@
 public class MovieTitleDto
 {
     public string ShowId { get; set; }
+    public string? Type { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string ImageUrl { get; set; }
     public int ReleaseYear { get; set; }
     public string Director { get; set; }
+    public string Cast { get; set; }
+    public string? Duration { get; set; }
+    public string? Country { get; set; }
     public string Genre { get; set; }
 }
